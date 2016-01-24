5832126581183e75f86d2e7627ba1c24a12e8bd3
Jobin <sibin.sun@gmail.com>
2016-01-24 14:44:42 +0800

    删掉数据库,并加入忽略文件,并完善后台显示

--- server/user/admin.py
+++ server/user/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
-from .models import Resume
-from .models import AuthCode
+from .models import User,Resume,AuthCode
 
-admin.site.register(User)
-admin.site.register(Resume)
-admin.site.register(AuthCode)
+class AuthCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'times')
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username','qq')
+    search_fields = ('email', 'username', 'qq')
+
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('userEmail','display', 'rank')
+    search_fields = ('userEmail','content')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Resume, ResumeAdmin)
+admin.site.register(AuthCode, AuthCodeAdmin)
