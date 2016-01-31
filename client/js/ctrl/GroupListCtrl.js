6f252224d23a51372cd4c750a3a28acfc5450307
wendy2003888 <wendyh2003888@gmail.com>
2016-01-31 23:29:59 +0800

    重写groupResumeList

--- client/js/ctrl/GroupListCtrl.js
+++ client/js/ctrl/GroupListCtrl.js
@@ -1,47 +1,51 @@
 angular.module('myApp')
-	.controller("GroupListCtrl",["$scope","$http",function($scope, $http){
+	.controller("GroupResumeListCtrl",["$scope", "$http", "$cookies", function($scope, $http, $cookies){
 		var list = {};
-		$http.get("/api/group/list/",{
-			params:{
-				code: $scope.code
-			}
-		}).success(function(response){
-			if (response.status == "success") {
-				$scope.items = []
-				try{
-					for(var i = 0; i < response.data.length; i++){
-						list[response.data[i].userEmail] = response.data[i];
-						$scope.items.push(response.data[i].userEmail);
-					}
-				}catch(e){}
-				$scope.show = true;
-			}else{
-				$T.toast(response.msg);
-			};
-		}).error(function(){
-			$T.toast("服务器错误,请联系系统管理员")
-		})
-		$scope.getInfo = function(key){
-			$scope.name = "loading...";
-			$scope.qq = "loading...";
-			$scope.content = list[key].content;
-			//现在没有authcode传入，所以暂时注释掉方便测试
-			// $http.get("/api/profile/",{
-			// 	params:{
-			// 		email: key,
-			// 		code: $scope.code
-			// 	}
-			// }).success(function(response){
-			// 	if(response.status == "success"){
-			// 		$scope.name = response.data.username;
-			// 		$scope.qq = response.data.qq;
-			// 	}else{
-			// 		$T.toast(response.msg)
-			// 	}
-			// }).error(function(){
-			// 	$scope.class = "red";
-			// 	$scope.name = "加载失败";
-			// 	$scope.qq = "加载失败";
-			// });
+		$scope.submit = function(){
+			$http.get("/api/group/resume_list/",{
+				params:{
+					groupID: $scope.groupID
+					//admin_token要怎么传？
+				}
+			}).success(function(response){
+				if (response.status == "success") {
+					$scope.items = []
+					try{
+						//这里直接从返回的data列表调用还是要再核对username？
+						for(var i = 0; i < response.data.length; i++){
+							// list[response.data[i].resumeId] = response.data[i];
+							if(response.data[i].status == 0)
+								response.data[i].status = "申请中";
+							else if(response.data[i].status == 1)
+								response.data[i].status = "允许的";
+							else if(response.data[i].status == 2)
+								response.data[i].status = "拒绝的";
+							else
+								response.data[i].status = "拉黑的";
+							$scope.items.push(response.data[i]);
+						}
+					}catch(e){}
+					$scope.show = true;
+				}else{
+					$T.toast(response.msg);
+				};
+			}).error(function(){
+				$T.toast("服务器错误,请联系系统管理员")
+			})
+		}
+		$scope.delResume = function(key){
+			$http.delete("/api/group/resume_list/", {
+				params:{
+					resumeId: key
+				}
+			}).success(function(response){
+				if(response.status == "success"){
+					$T.toast("简历已删除");
+				}else{
+					$T.toast(response.msg);
+				}
+			}).error(function(){
+				$T.toast("服务器错误,请联系系统管理员")
+			})
 		}
 	}])
--- server/api/controller/group/resume_list.py
+++ server/api/controller/group/resume_list.py
@@ -70,18 +70,98 @@ delete:
 
 from django.http import JsonResponse
 from django.views.generic import View
+from django.db.models import Avg
+from django.forms import (Form, CharField, IntegerField, ChoiceField)
+
+from .check_request import CheckRequest
+from api.models import Resume, Rank, User
+
+
+class putForm(Form):
+    STATUS_CHOICES = (
+        (0,u'申请中'),
+        (1,u'允许的'),
+        (2,u'拒绝的'),
+        (3,u'拉黑的'),
+    )
+    resumeId = IntegerField()
+    status = ChoiceField(choices=STATUS_CHOICES, required=False)
+    rank = IntegerField(required=False)
+
+class DelForm(Form):
+    resumeId = IntegerField()
 
 class Index(View):
     def get(self, request):
-        return JsonResponse({"status":"success",
-                             "msg":""})
+        check = CheckRequest(request)
+        if not check.admin:
+            return JsonResponse({"status": "error",
+                                "msg": "Only admin permitted"})
+        data = {"status" :  "success",
+                "msg" :  '',
+                "data" : []
+                }
+        resumes = Resume.objects.filter(groupID = check.admin.groupID)
+        for item in resumes:
+            print('item', item)
+            user = User.objects.filter(email = item.userEmail).first()
+            allRank = Rank.objects.filter(resumeId = item.id)
+            rank = allRank.filter(adminName = check.admin.adminName).first()
+            avgRank = allRank.aggregate(Avg('rank'))
+            if not user:
+                return JsonResponse({"status": "error",
+                                    "msg": "Resume without valid user"})
+            resume = {
+                "resumeId": item.id,
+                "groupId": item.groupID,
+                "username": user.username,
+                "qq": item.qq,
+                "lastDate": item.lastDate.strftime('%Y-%m-%d'),
+                "content": item.content,
+                "status": item.status
+            }
+            if not rank: 
+                resume['myRank'] = u'尚未评分'
+            else:
+                resume['myRank'] = rank.rank
+            #不知这个是用户的averageRank还是简历的averageRank?现在是简历的averageRank
+            resume['averageRank'] = avgRank['rank__avg']
+            data['data'].append(resume)
+        return JsonResponse(data)
+        
     def post(self, request):
         return JsonResponse({"status":"success",
                              "msg":""})
 
     def put(self, request):
+        check = CheckRequest(request)
+        if not check.admin:
+            return JsonResponse({"status": "error",
+                                "msg": "Only admin permitted"})
+        uf = DelForm(check.jsonForm)
+        if not uf.is_valid():
+            return JsonResponse({"status": "error",
+                                "msg": "resumeId is invalid."})
+        resume = Resume.objects.filter(id = uf.cleaned_data['resumeId']).first()
+        if uf.cleaned_data['status']:
+            resume.status = uf.cleaned_data['status']
+        if uf.cleaned_data['rank']:
+            resume.rank = uf.cleaned_data['rank']
+        resume.save()
         return JsonResponse({"status":"success",
                              "msg":""})
+
     def delete(self, request):
+        #删除部分是不是有二次确认比较好？
+        check = CheckRequest(request)
+        if not check.admin:
+            return JsonResponse({"status": "error",
+                                "msg": "Only admin permitted"})
+        uf = DelForm(check.jsonForm)
+        if not uf.is_valid():
+            return JsonResponse({"status": "error",
+                                "msg": "resumeId is invalid."})
+        Resume.objects.filter(id = uf.cleaned_data['resumeId']).delete()
+        Rank.objects.filter(resumeId = uf.cleaned_data['resumeId']).delete()
         return JsonResponse({"status":"success",
                              "msg":""})
