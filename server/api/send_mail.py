6b48cad6276eb208878632b286a274176dc21e96
FPlust Lu <fplustlu@gmail.com>
2016-03-29 02:20:34 +0800

    简化代码

--- server/api/send_mail.py
+++ server/api/send_mail.py
@@ -14,12 +14,13 @@ def start_mail_thread(subject, message, receivers):
 
     msg = MIMEText(message, 'html', 'utf-8')
     h = Header(subject, c)
+    sender = settings.DEFAULT_FROM_EMAIL
     msg['Subject'] = h
-    msg['From'] = "=?utf-8?b?%s?= <%s>" % (b64encode("QJob社交招聘".encode("utf-8")).decode("utf-8"), settings.DEFAULT_FROM_EMAIL)
+    msg['From'] = "=?utf-8?b?%s?= <%s>" % (b64encode("QJob社交招聘".encode("utf-8")).decode("utf-8"), sender)
     msg['To'] = ','.join(receivers)
 
     smtp = SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
-    smtp.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+    smtp.login(settings.EMAIL_HOST_USER, sender)
 
     new_thread = Thread(target=smtp.sendmail, args=[settings.DEFAULT_FROM_EMAIL, receivers, msg.as_string()], daemon=True)
     new_thread.start()
